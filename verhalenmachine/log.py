97b398373318496ecc21eafea899c3d236e1a3cd
Erwin Elling <erwinelling@gmail.com>
2019-02-25 22:00:35 +0100

    added log.py

---
+++ verhalenmachine/log.py
@@ -0,0 +1,26 @@
+import logging
+
+HOME_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
+print HOME_DIR
+LOG_FILE = os.path.join(HOME_DIR, "verhalenmachine.log")
+
+def setup_custom_logger(name):
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+
+    # handler = logging.StreamHandler()
+    # handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
+    fh = logging.handlers.RotatingFileHandler(
+                  LOG_FILE, maxBytes=5000000, backupCount=5)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    return logger
