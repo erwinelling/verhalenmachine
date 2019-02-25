ce8c5633ee13fd8eeba5602a5deb64bd9a9a421b
Erwin Elling <erwinelling@gmail.com>
2019-02-25 20:54:23 +0100

    typo fix

--- verhalenmachine.py
+++ verhalenmachine.py
@@ -112,7 +112,53 @@ class VolumioClient:
         self._client.emit('play')
 
         ### TODO: added stuff for vu meter here for now, refactor later
-
+        # See https://github.com/erwinelling/verhalenmachine/blob/e6837b1d92393c15df3c352ad5fc2e09d919adb1/vumeter.py
+        # Open the FIFO that MPD has created for us
+        self.vu.test()
+        # # This represents the sample (44100:16:2) that MPD is currently "playing"
+        # fifo = os.open('/tmp/mpd.fifo', os.O_RDONLY)
+        # ser = serial.Serial("/dev/ttyUSB0", baudrate=57600, timeout=1.0)
+        #
+        # ser.write(str(1)+"\r")
+        # while True:
+        #     try:
+        #         rawStream = os.read(fifo, 4096)
+        #     except OSError as err:
+        #         if err.errno == errno.EAGAIN or err.errno == errno.EWOULDBLOCK:
+        #             rawStream = None
+        #         else:
+        #             raise
+        #
+        #     just_after_stream = False
+        #     if rawStream:
+        #         # TODO: Change to mono signal
+        #         # TODO: Change dB scale to 0-100 scale
+        #
+        #             # leftChannel = audioop.tomono(rawStream, 2, 1, 0)
+        #             # rightChannel = audioop.tomono(rawStream, 2, 0, 1)
+        #             stereoPeak = audioop.max(rawStream, 2)
+        #             # leftPeak = audioop.max(leftChannel, 2)
+        #             # rightPeak = audioop.max(rightChannel, 2)
+        #             # leftDB = 20 * math.log10(leftPeak) -74
+        #             # rightDB = 20 * math.log10(rightPeak) -74
+        #
+        #             # print(rightPeak, leftPeak, rightDB, leftDB)
+        #
+        #             out = stereoPeak/40
+        #             if out > 100:
+        #                 out = 100
+        #             if out < 1:
+        #                 out = 1
+        #             print out
+        #             ser.write(str(out)+"\r")
+        #             just_after_stream = True
+        #             time.sleep(0.05)
+        #     else:
+        #         # TODO: Test
+        #         # Turn meter down when stream stops
+        #         if just_after_stream:
+        #             ser.write("1\r")
+        #             just_after_stream = False
 
     def pause(self):
         self._client.emit('pause')
@@ -441,7 +487,7 @@ class Recorder:
 class VU:
     def __init__(self, pin):
         # Pin Setup:
-        self.pin = pinf
+        self.pin = pin
 
         # Initiate VU
         GPIO.setmode(GPIO.BOARD)  # Broadcom pin-numbering scheme
